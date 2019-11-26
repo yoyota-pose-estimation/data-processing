552889eeb758fea2ca46c34499d94db5506b47a7
yoyota <smarthorse6@gmail.com>
2019-11-27 08:57:35 +0900

    Delete k8s things

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pair-image",
-    package_dir={"": "src"},
-    packages=find_packages("src"),
-    install_requires=["tensorflow>=2.0.0", "kubernetes>=10.0.1"],
+    name="pose-estimation-data-processing",
+    install_requires=["tensorflow>=2.0.0"],
     entry_points={"console_scripts": ["pair_image=pair_image.cli:main"]},
 )
